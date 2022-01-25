267086fdbd9c9305bf9e38694e1ed003ca100496
onlywyt <17600187623@163.com>
2022-01-25 11:10:46 +0800

    线程池监控demo

---
+++ src/main/java/com/example/logbackdemo/com/test/source/thread/threadpool/monitor/ThreadPoolEndpoint.java
@@ -0,0 +1,52 @@
+package com.example.logbackdemo.com.test.source.thread.threadpool.monitor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @program: source-demo
+ * @description:
+ * @ClassName：ThreadPoolEndpoint
+ * @author: Mr.Wang
+ * @create: 2022-01-24 22:52
+ **/
+@Configuration
+@Endpoint(id="thread-pool")
+public class ThreadPoolEndpoint {
+    @Autowired
+    private ThreadPoolForMonitorManager threadPoolForMonitorManager;
+
+    @ReadOperation
+    public Map<String,Object> threadPoolsMetrics(){
+        Map<String,Object> metricsMap = new HashMap<>();
+        List<Map> threadPools = new ArrayList<>();
+
+        threadPoolForMonitorManager.getThreadPoolMap().forEach((k,v)->{
+            ThreadPoolExecutorMonitor tpe =  v;
+            Map<String,Object> poolInfo = new HashMap<>();
+            poolInfo.put("thread.pool",k);
+            poolInfo.put("thread.pool.core.size",tpe.getCorePoolSize());
+            poolInfo.put("thread.pool.largest.size",tpe.getMaximumPoolSize());
+            poolInfo.put("thread.pool.thread.count",tpe.getPoolSize());
+            poolInfo.put("thread.pool.min.costTime",tpe.getMinCostime());
+            poolInfo.put("thread.pool.average.costTime",tpe.getActualCostime());
+            poolInfo.put("thread.pool.max.costTime",tpe.getMaxCostime());
+            poolInfo.put("thread.pool.active.count",tpe.getActiveCount());
+            poolInfo.put("thread.pool.completed.taskCount",tpe.getCompletedTaskCount());
+            poolInfo.put("thread.pool.queue.name",tpe.getQueue().getClass().getName());
+            poolInfo.put("thread.pool.rejected.name",tpe.getRejectedExecutionHandler().getClass().getName());
+            poolInfo.put("thread.pool.task.count",tpe.getTaskCount());
+            threadPools.add(poolInfo);
+        });
+        metricsMap.put("threadPools",threadPools);
+        return metricsMap;
+    }
+}
---
+++ src/main/java/com/example/logbackdemo/com/test/source/thread/threadpool/monitor/ThreadPoolProperties.java
@@ -0,0 +1,23 @@
+package com.example.logbackdemo.com.test.source.thread.threadpool.monitor;
+
+import lombok.Data;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: source-demo
+ * @description:
+ * @ClassName：ThreadPoolProperties
+ * @author: Mr.Wang
+ * @create: 2022-01-24 22:26
+ **/
+@Data
+public class ThreadPoolProperties {
+    private String poolName;
+    private int corePoolSize;
+    private int maximumPoolSize=Runtime.getRuntime().availableProcessors();
+    private long keepAliveTime=60;
+    private TimeUnit unit=TimeUnit.SECONDS;
+    private int queueCapacity=Integer.MAX_VALUE;
+
+}
