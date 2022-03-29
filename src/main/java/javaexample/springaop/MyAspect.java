cb85e190781c5783f35b8a0d4cdac43407f3d465
onlywyt <17600187623@163.com>
2022-03-29 22:23:57 +0800

    tijiao

--- src/main/java/javaexample/springaop/MyAspect.java
+++ src/main/java/javaexample/springaop/MyAspect.java
@@ -1,12 +1,52 @@
-package javaexample.springaop;/**
-* @program: source-demo
-*
-* @description: 
+package javaexample.springaop;
 
-* @ClassName：MyAspect
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 
-* @author: Mr.Wang
-*
-* @create: 2022-03-15 16:13
-**/public class MyAspect {
+import java.util.Arrays;
+
+/**
+ * @program: source-demo
+ * @description:
+ * @ClassName：MyAspect
+ * @author: Mr.Wang
+ * @create: 2022-03-15 16:13
+ **/
+@Aspect
+@Component
+public class MyAspect {
+
+
+    @Pointcut("execution(public * javaexample.springaop.TargetClass.test(..))")
+    public void pointcut(){
+
+    }
+
+    @Before("pointcut()")
+    public void onBeforeMethod(JoinPoint joinPoint){
+        System.out.println("onBefore:"
+                + joinPoint.getSignature().getName()
+                + "方法开始执行，参数:"
+                + Arrays.toString(joinPoint.getArgs()));
+    }
+    @After("pointcut()")
+    public void onAfter(JoinPoint joinPoint) {
+        System.out.println("onAfter：" + joinPoint.getSignature().getName() + "方法执行结束，参数："
+                + Arrays.asList(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(value = "pointcut()", returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+        System.out.println("afterReturning：" + joinPoint.getSignature().getName() + "方法执行结束返回，参数："
+                + Arrays.asList(joinPoint.getArgs()) + "，返回值：" + result);
+    }
+
+    @AfterThrowing(value = "pointcut()", throwing = "exception")
+    public void afterThrowing(JoinPoint joinPoint, Exception exception) {
+        System.out.println("afterThrowing：" + joinPoint.getSignature().getName() + "方法执行出错，参数："
+                + Arrays.asList(joinPoint.getArgs()) + "，异常：" + exception);
+    }
+
+
 }
--- src/main/java/javaexample/springaop/TargetClass.java
+++ src/main/java/javaexample/springaop/TargetClass.java
@@ -1,12 +1,22 @@
-package javaexample.springaop;/**
-* @program: source-demo
-*
-* @description: 
+package javaexample.springaop;
 
-* @ClassName：TargetClass
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
-* @author: Mr.Wang
-*
-* @create: 2022-03-15 16:12
-**/public class TargetClass {
+/**
+ * @program: source-demo
+ * @description:
+ * @ClassName：TargetClass
+ * @author: Mr.Wang
+ * @create: 2022-03-15 16:12
+ **/
+@Component
+public class TargetClass {
+    public String test(String value){
+        System.out.println("目标方法test被执行");
+        if (!StringUtils.hasLength(value)){
+            throw new RuntimeException("value 不能为null");
+        }
+        return value;
+    }
 }
